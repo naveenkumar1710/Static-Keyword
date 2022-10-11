2d813f441bad7466198d3641196662d25ecd9d80
naveenkumar1710 <113573484+naveenkumar1710@users.noreply.github.com>
2022-10-11 13:04:31 +0530

    Add files via upload

---
+++ KeywordStatic.java
@@ -0,0 +1,22 @@
+public class KeywordStatic{
+
+        static int no=123;
+        static String name="naveen";
+    static void payilagam(){
+        System.out.println(name);
+        }
+        
+    static void courseName(){
+        name="sugu";
+        System.out.println(no);
+        System.out.println(name);
+        }
+        
+        public static void main(String[]args){
+        
+        
+        payilagam();
+        courseName();
+        
+}
+}
